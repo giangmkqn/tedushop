3b54e481e3704363afe889fe5c183859d015f685
giangmkqn <giangmkios@gmail.com>
2019-01-23 09:27:17 +0700

    23/1 Commit

--- TeduShop.Data/InfraStructure/IRepository.cs
+++ TeduShop.Data/InfraStructure/IRepository.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TeduShop.Data.InfraStructure
 {
-    interface IRepository
+    public interface IRepository<T> where T : class
     {
+        //Mark as entity as new
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void DeleteMulti(Expression<Func<T, bool>> where);
+
+        T GetSingleByID(int id);
+
+        T GetSingleByCondition(Expression<Func<T, bool>> expression, string[] includes = null);
+
+        IQueryable<T> GetAll(string[] includes = null);
+
+        IQueryable<T> GetMulti(Expression<Func<T, bool>> predicate, string[] includes = null);
+
+        IQueryable<T> GetMultiPaging(Expression<Func<T, bool>> filter, out int total, int index = 0);
+
+        int Count(Expression<Func<T, bool>> where);
+
+        bool CheckContains(Expression<Func<T, bool>> predicate);
     }
 }
